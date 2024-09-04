5f115a3cef103d8d71899908390af7253d6459d6
Nurmash <nurmashfg@gmail.com>
2024-09-05 01:44:39 +0900

    New version

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
 #include <chrono>  
 #include <string>  
+#include <conio.h>
 
 int main() {
-    const char* clear;
-
-    #ifdef _WIN32
-        clear = "cls";
-    #else 
-        clear = "clear";
-    #endif
-    
+    system("color 02");
     std::string testText[] = {
         "Emma hurried through the park, her eyes scanning the ground.", 
         "She had just realized she lost her house key somewhere on her walk,", 
@@ -26,60 +20,72 @@ int main() {
     };
 
     size_t count = 0;
+    bool inputCorrect = true;
     
     while (true) {
+        std::string userInput;
         std::cout << testText[count] << std::endl;
+
         for (size_t i = 0; i < testText[count].size(); i++) {
             std::cout << "-";
         }
         std::cout << "\n";
-
-        std::string userInput;
-        bool inputCorrect = true;
-
        
         auto start = std::chrono::steady_clock::now();
         
-        std::getline(std::cin, userInput);
-        
-        auto end = std::chrono::steady_clock::now();
-        std::chrono::duration<double> elapsed_seconds = end - start;
-        if (userInput == "exit") {
-            break;
-        }
-        
-        for (size_t i = 0; i < userInput.size(); i++) {
-            if (userInput[i] != testText[count][i] && userInput[i] != ' ') {
-                std::cout << "^";
-                inputCorrect = false;
+        size_t position = 0;
+        bool inputCorrect = true;
+
+        while (position < testText[count].size()) {
+            char ch = _getch();
+
+            if (ch == '\b' && position > 0) {
+                std::cout << "\b \b";
+                userInput.pop_back();
+                position--;
+            } else if (ch != '\b' && ch != '\r') {
+                std::cout << ch;
+                userInput += ch;
+                position++;
+            }
+
+            for (size_t i = 0; i < userInput.size(); i++) {
+                if (testText[count][i] != userInput[i]) {
+                    inputCorrect = false;
+                    break;
+                }
+                if (i == userInput.size() - 1 && testText[count][i] == userInput[i]) {
+                    inputCorrect = true;
+                    break;
+                }
+            }
+            
+            if (inputCorrect == true) {
+                system("color 02");
             } else {
-                std::cout << " ";
+                system("color 04");
             }
         }
-        if (userInput.size() < testText[count].size()) {
-            std::cout << "^";
-            inputCorrect = false;
-        }
-        
-        std::cout << "\n";
 
-        if (inputCorrect == true) {
-            int numChars = userInput.size();
-            double charsPerSecond = numChars / elapsed_seconds.count();
-            double charsPerMinute = charsPerSecond * 60;
+        auto end = std::chrono::steady_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
 
-            system(clear);
+        int numChars = userInput.size();
+        double charsPerSecond = numChars / elapsed_seconds.count();
+        double charsPerMinute = charsPerSecond * 60;
 
+        system("cls");
+
+        if (inputCorrect) {
             std::cout << "Time taken: " << elapsed_seconds.count() << " seconds\n";
             std::cout << "Characters per minute: " << charsPerMinute << "\n\n";
-
             if (count == 10) {
                 count = 0;
             } else {
                 count++;
             }
-        }
+        } 
+        inputCorrect = true;
     }
-
     return 0;
 }
